432a81b91bb52ad7a3f3f9109fef55c24ef14fb0
張君皓 MUST <93756163+EGG0507@users.noreply.github.com>
2021-11-11 21:24:51 +0800

    Create 1.html.php

---
+++ 1.html.php
@@ -0,0 +1,16 @@
+<html>
+    <head><title>html+php</title></head>      
+    <body>
+        <?php echo "Hello world!"; 
+        //利用echo來列印後面的文字
+        ?> 
+        <h1>html</h1>
+        <?php echo "Hello world!"; 
+        //利用echo來列印後面的文字
+        ?> 
+        <h1>html</h1>
+        <?php echo "Hello world!";
+        //利用echo來列印後面的文字
+        ?> 
+    </body>
+</html>
